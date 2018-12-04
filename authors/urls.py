e2f6025368ae783eff30495c8a844145c0912c76
Risper Kemunto <remmy@Rispers-MacBook-Pro.local>
2018-12-04 14:24:04 +0300

    chore(test-registration): Test user registration
    -Add .DS_Store,.vscode and virtual folder to .gitignore
    -Create test for user registration
    -Fix routing errors
    -Add input validation tests
    -Add test to check for duplicate data
    -Test for missing data, valid username and password
    -Add tests for missing parameters
    -Configure code to pep8 standards
    -Generate user token
    
    [Starts #161966897]

--- authors/urls.py
+++ authors/urls.py
@@ -18,5 +18,6 @@ from django.contrib import admin
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api-auth/', include('rest_framework.urls', namespace='rest_framework')),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_\
+    framework')),
 ]
