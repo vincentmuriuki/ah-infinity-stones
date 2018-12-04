2a633d2982d6c80fc9d3571bf2e9e672f3d73701
PromasterGuru <39240075+PromasterGuru@users.noreply.github.com>
2018-12-04 13:12:20 +0300

    chore(configure-postgresql-db): Configure Authors Haven to use PostgreSQL as DB Engine (#16)
    - ensure Authors Haven is configured to use PostgreSQL as DB Engine instead of the default sqlite DB
    
    [Delivers #161966905]

--- authors/settings.py
+++ authors/settings.py
@@ -82,9 +82,16 @@ WSGI_APPLICATION = 'authors.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DATABASE_NAME', ''),
+        'USER': os.environ.get('DATABASE_USER', ''),
+        'PASSWORD': os.environ.get('DATABASE_PASSWORD', ''),
+        'HOST': '',
+        'PORT': '5432',
+        'TEST': {
+            'NAME': os.environ.get('DATABASE_TEST', '')
+        },
+    },
 }
 
 # Password validation
