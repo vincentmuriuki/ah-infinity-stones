dfcfbe737e886f3185db6d271d62639f76be4aae
PromasterGuru <39240075+PromasterGuru@users.noreply.github.com>
2018-12-04 09:54:34 +0300

    fix(django-upgrade): Fix Django upgrade issues (#15)
    - Ensure all the requirements for Django 2 are met in the code
    
    [Finishes #161966900]

--- authors/apps/authentication/views.py
+++ authors/apps/authentication/views.py
@@ -1,5 +1,5 @@
 from rest_framework import status
-from rest_framework.generics import RetrieveUpdateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
 from rest_framework.permissions import AllowAny, IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
