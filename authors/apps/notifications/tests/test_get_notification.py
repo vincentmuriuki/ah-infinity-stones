2f822ce7b6145a73fdd0d088e9b5104e41782aaa
Leo <leonard.njura@gmail.com>
2018-12-07 12:56:51 +0300

    [CHORE #161966897] added update and delete test cases

--- authors/apps/notifications/tests/test_get_notification.py
+++ authors/apps/notifications/tests/test_get_notification.py
@@ -69,3 +69,47 @@ class TestNotificationView(BaseSetUp):
         response = self.client.get(
             "/api/notifications/100001", format="json")
         self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
+class NotificationTestCaseDeleteUpdate(BaseSetUp):
+    """Tests class for the delete and update"""
+    
+    def test_can_update_one_notification(self):
+        """
+        Test can update a notifications
+        """
+        self.data = {"read": True}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.put(
+            "/api/notifications/1", self.data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+
+    def test_cannot_update_unexisting_notification(self):
+        """
+        Test cannot update a notifications
+        """
+        self.data = {"read": True}
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.put(
+            "/api/notifications/100000001", self.data, format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_can_delete_one_notification(self):
+        """
+        Test can delete a notification
+        """
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(
+            "/api/notifications/1", format="json")
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+
+    def test_cannot_delete_unexisting_notification(self):
+        """
+        Test cannot delete a notification
+        """
+        self.client.credentials(HTTP_AUTHORIZATION='Token ' + self.token)
+        response = self.client.delete(
+            "/api/notifications/100000001", format="json")
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+
