dce6e9514a7b45e6559a445a478ac363a5a6f292
rkemmy <rkemmy.rk69@gmail.com>
2018-12-17 06:58:19 +0300

    create an article and retrieve a specific article by id

--- authors/apps/articles/serializers.py
+++ authors/apps/articles/serializers.py
@@ -1,6 +1,11 @@
 from rest_framework import serializers
 from authors.apps.articles.models import Article, User, Tag, Comment
+<<<<<<< HEAD
 from taggit_serializer.serializers import (TagListSerializerField)
+=======
+from taggit_serializer.serializers import (TagListSerializerField,
+                                           TaggitSerializer)
+>>>>>>> create an article and retrieve a specific article by id
 
 
 class UserSerializer(serializers.ModelSerializer):
@@ -15,15 +20,22 @@ class TagSerializer(serializers.ModelSerializer):
 
 
 class ArticleSerializer(serializers.ModelSerializer):
+<<<<<<< HEAD
     """Article serializer that converts querysets to json data"""
+=======
+>>>>>>> create an article and retrieve a specific article by id
     user = serializers.ReadOnlyField(source='user.username')
     tag = TagListSerializerField()
 
     class Meta:
 
         model = Article
+<<<<<<< HEAD
         fields = ("art_slug", "title", "description", "body", "read_time",
                   "tag", "user", "created_at", "updated_at")
+=======
+        fields = ("title", "description", "body", "read_time", "user", "tag",)
+>>>>>>> create an article and retrieve a specific article by id
 
 
 class CommentSerializer(serializers.ModelSerializer):
